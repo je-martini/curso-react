444b6cedec8db452107788686f9c3a4f0e687a5b
je_martini <jesusmartini2017@gmail.com>
2024-02-19 16:45:05 -0500

    setup

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edita el archivo <code>src/App.js</code> y guarda para recargar.
+          Edita eeel archivo <code>src/App.js</code> y guarda para recargar.
         </p>
         <a
           className="App-link"
