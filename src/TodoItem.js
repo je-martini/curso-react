092530cda40d5192ac2d21e7053a6cbbfcd569a2
je_martini <jesusmartini2017@gmail.com>
2024-02-26 08:37:09 -0500

    adding styles to all the app

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -1,11 +1,15 @@
+import './TodoItem.css';
+
 function TodoItem(props){
     return(
-      <li>
-        <span>v
+      <li className='TodoItem'>
+        <span className='Icon Icon-check Icon-check--active'>v
+          V
           {/* {props.completed} */}
           </span>
-        <p>{props.text}</p>
-        <span>x</span>
+        <p className='TodoItem-p TodoItem-p--complete'>
+          {props.text}</p>
+        <span className='Icon Icon-delete'>x</span>
       </li>
     );
 }
