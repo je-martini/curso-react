a0b3c0930408155da44e0d0803cf05ede1cc92f6
je_martini <jesusmartini2017@gmail.com>
2024-02-26 08:44:27 -0500

    adding interactive style

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -3,11 +3,12 @@ import './TodoItem.css';
 function TodoItem(props){
     return(
       <li className='TodoItem'>
-        <span className='Icon Icon-check Icon-check--active'>v
+        <span className={`Icon Icon-check 
+        ${props.completed && 'Icon-check--active'}`}>v
           V
           {/* {props.completed} */}
           </span>
-        <p className='TodoItem-p TodoItem-p--complete'>
+        <p className={`TodoItem-p ${props.completed && "TodoItem-p--complete"}`}>
           {props.text}</p>
         <span className='Icon Icon-delete'>x</span>
       </li>
