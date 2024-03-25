2da402c666d5377871f2f8bbfe0e3e6478ca1b4d
je_martini <jesusmartini2017@gmail.com>
2024-03-25 08:11:45 -0400

    adding react context

--- src/App/AppUI.js
+++ src/App/AppUI.js
@@ -6,60 +6,64 @@ import { CreateTodoButton } from '../CreateTodoButton';
 import { TodosLoanding } from '../TodosLoading';
 import { TodosError } from '../TodosError';
 import { EmptyTodos } from '../EmptyTodos';
+import { TodoContext } from '../TodoContext';
 import React from 'react';
 
-function AppUI({
-    loading,
-    error,
-    completedTodos,
-    totalTodos,
-    searchValue,
-    setSearchValue,
-    searchedTodos,
-    completeTodo,
-    deleteTodo,
-}) {
+function AppUI() {
     return (
         <React.Fragment>
     
           <TodoCounter 
-            completed={completedTodos} 
-            total={totalTodos}
+            // completed={completedTodos} 
+            // total={totalTodos}
           />
     
           <TodoSearch 
-            searchValue={searchValue}
-            setSearchValue={setSearchValue}
+            // searchValue={searchValue}
+            // setSearchValue={setSearchValue}
           />
     
-          <TodoList >
+          <TodoContext.Consumer>
+            { ({
+              loading,
+              error,
+              completedTodos,
+              searchValue,
+              searchedTodos,
+              totalTodos,
+              setSearchValue,
+              completeTodo,
+              deleteTodo,
+            }) => (
+            <TodoList >
+              { loading && (<>
+              <TodosLoanding/>
+              <TodosLoanding/>
+              <TodosLoanding/>
+              </>)}
+              { error && <TodosError/>}
+              {(!loading && searchedTodos.length === 0) &&
+              (<>
+              <EmptyTodos/>
+              <EmptyTodos/>
+              <EmptyTodos/>
+              </>)
+              }
 
-          { loading && (<>
-          <TodosLoanding/>
-          <TodosLoanding/>
-          <TodosLoanding/>
-          </>)}
-          { error && <TodosError/>}
-          {(!loading && searchedTodos.length === 0) &&
-           (<>
-           <EmptyTodos/>
-           <EmptyTodos/>
-           <EmptyTodos/>
-           </>)
-           }
-
-            {searchedTodos.map(todo => (
-              
-              <TodoItem 
-                key={todo.text}
-                text={todo.text}
-                completed={todo.completed}
-                onComplete = {() => completeTodo(todo.text)}
-                onDelete = {() => deleteTodo(todo.text)}
-                
-              />
-            ))}
-          </TodoList>
+                {searchedTodos.map(todo => (
+                  
+                  <TodoItem 
+                    key={todo.text}
+                    text={todo.text}
+                    completed={todo.completed}
+                    onComplete = {() => completeTodo(todo.text)}
+                    onDelete = {() => deleteTodo(todo.text)}
+                    
+                  />
+                ))}
+            </TodoList>
+            )}
+          </TodoContext.Consumer>
     
           <CreateTodoButton />
     
